ffee500e03cb13517f96d3806f2a83fa5df441ac
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 23:32:56 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
   
   def create
+    user = User.new(user_params).save
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
