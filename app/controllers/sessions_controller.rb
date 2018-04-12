417a461092db4e344a8d8d0f8329efbd54eeec2f
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 23:30:43 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    flash[:alert] = "Incorrect Password." unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+  end
 end
