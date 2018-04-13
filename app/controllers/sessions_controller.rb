0ccf17c968f4ff153c20a25520b612e5020947a2
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 00:51:33 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:user][:name])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      @user = user
-      redirect_to controller: 'welcome', action: 'index'
-    else
-      flash[:alert] = "Incorrect Password."
-      redirect_to controller: 'sessions', action: 'new'
-    end
-  end
+
+   user = user.try(:authenticate, params[:user][:password])
+
+   return redirect_to(controller: 'sessions', action: 'new') unless user
+
+   session[:user_id] = user.id
+
+   @user = user
+
+   redirect_to controller: 'welcome', action: 'home'
+ end
 end
