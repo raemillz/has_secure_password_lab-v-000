e0800b6a749def600c009fba1e94e2e3560a38eb
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 00:32:00 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412232006) do
+ActiveRecord::Schema.define(version: 20180413003013) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
