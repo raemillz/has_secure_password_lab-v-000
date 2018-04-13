df9419d7baf6040592b0562268e03ae8f4dd6168
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 00:51:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 
    @user = user
 
-   redirect_to controller: 'welcome', action: 'home'
+   redirect_to controller: 'welcome', action: 'index'
  end
  
 end
