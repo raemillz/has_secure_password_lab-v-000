816cea8b736803443b8ac6356d0540d428c804db
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 00:49:04 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = @user.id
       @user = user
-      redirect_to user_path(@user)
+      redirect_to controller: 'welcome', action: 'index'
     else
       flash[:alert] = "Incorrect Password."
       redirect_to controller: 'sessions', action: 'new'
