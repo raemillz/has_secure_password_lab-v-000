099c990eb5ce54f2eb6cd45093e7fa949150cb6f
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 00:10:04 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
 
-  get 'sessions/new'
+  #get 'sessions/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
