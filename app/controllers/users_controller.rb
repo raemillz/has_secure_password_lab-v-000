01f9ecaf5a435d182c517b446fbd9768b5f8c9a4
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 01:01:36 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,7 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
