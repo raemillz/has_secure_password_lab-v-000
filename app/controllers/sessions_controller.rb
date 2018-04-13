abcc9877824ddf1c47420781be0f75f0b62b3e79
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 00:33:36 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to user_path(@user)
     else
       flash[:alert] = "Incorrect Password."
-      redirect_to user
+      redirect_to new_session_path
     end
   end
 end
