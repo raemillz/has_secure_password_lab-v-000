8fc2df2ea6b6b14464e9231986a92911d6de6efb
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 01:12:16 +0000

    automatically backed up by learn

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   before_action :require_logged_in
 
   def index
-    @user = User.find_by(params[:user][:id])
+    @user = User.find_by(id: params[:user][:id])
   end
 
 end
